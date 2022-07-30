52a701560415df8ae9fbfe18bfadccf40319c400
LieNautz <tuan_lai01@hotmail.com>
2022-07-30 19:37:48 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+// describe('index.js', function () {
+//     describe('companyName', function () {
+//       it('is set as Scuber', function () {
+//         expect(companyName).to.equal('Scuber');
+//       });
+  
+const companyName= "Scuber";
+
+// describe('mostProfitableNeighborhood', function () {
+//     it('is declared as equal to Chelsea', function () {
+//       expect(mostProfitableNeighborhood).to.equal('Chelsea');
+//     });
+
+let mostProfitableNeighborhood= 'Chelsea';
+
+// describe('companyCeo', function () {
+//     it('is declared as equal to Susan Smith', function () {
+//       expect(companyCeo).to.equal('Susan Smith');
+//     });
+
+let companyCeo ="Susan Smith"
